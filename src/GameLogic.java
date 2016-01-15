8885aa1310f656b57f2bbf8f3062eab926c08857
AgX-1 <agx-1@xeroserver.org>
2016-01-15 21:11:30 +0100

    Added a Territory interface and started implementing the attack() function of the GameLogic class

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,10 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Created by Moritz on 15.01.2016.
  */
 public class GameLogic {
 
-    public static void main(String[] args) {
+    //attacker has to have >1 armies in his Territory
+    public void attack(Territory attacker, Territory defendant)
+    {
+        int[] attackerDices = new int[3];
+        int[] defendantDices = new int[2];
+        int iterAttack = 0;
+        int iterDefend = 0;
 
-        System.out.println("Hello Morian");
+        while (attacker.getArmys() > 1 && iterAttack < 3){
+
+            attackerDices[iterAttack] = rollDice();
+            iterAttack++;
+        }
+
+        while (defendant.getArmys() > 0 && iterDefend < 2){
+
+            defendantDices[iterDefend] = rollDice();
+            iterDefend++;
+        }
+
+        Arrays.sort(attackerDices);
+        Arrays.sort(defendantDices);
+
+        //just for testing, remove if finished
+        System.out.println("Attacker Array: ");
+        for (int i = 0; i < attackerDices.length; i++) {
+
+            System.out.println(attackerDices[i] + ", ");
+        }
+
+        System.out.println("Defendant Array: ");
+        for (int i = 0; i < defendantDices.length; i++) {
+
+            System.out.println(defendantDices[i] + ", ");
+        }
+    }
+
+
+    private int rollDice(){
+
+        return ((int)(Math.random()*6))%6 + 1;
     }
 }
---
+++ src/Territory.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+/**
+ * Created by fabian on 15.01.16.
+ */
+public interface Territory {
+
+    //get the number of armies in current territory
+    int getArmys();
+
+    //set the number of armies in current territory
+    int setArmys();
+
+}
